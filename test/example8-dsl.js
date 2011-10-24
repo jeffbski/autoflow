c53fdc49637ad0fb3d7402d4a7a4e5354586018e
Jeff Barczewski <jeff.barczewski@gmail.com>
2011-10-24 15:16:11 -0500

    add support for using object methods in addition to functions function can be a string with a dot instead of a function ref
    { f: 'obj.method' } for method
    { f: myfun } for function

--- test/example8-dsl.js
+++ test/example8-dsl.js
@@ -1,11 +1,19 @@
 'use strict';
 
+/**
+  Using object methods in addition to functions with simple DSL
+ */
+
 var react = require('../react.js').react;
 var reactOptions = require('react').reactOptions;
 reactOptions.debugOutput = true;
 reactOptions.stackTraceLimitMin = 20;
 
-function loadUser(uid, cb){ setTimeout(cb, 100, null, "User"+uid); }
+var UserMgr = function(){ };
+UserMgr.prototype = {};
+UserMgr.prototype.constructor = UserMgr;
+UserMgr.prototype.loadUser = function loadUser(uid, cb){ setTimeout(cb, 100, null, "User"+uid); };
+
 function loadFile(filename, cb){ setTimeout(cb, 100, null, 'Filedata'+filename); }
 function markdown(filedata) { return 'html'+filedata; }
 function prepareDirectory(outDirname, cb){ setTimeout(cb, 200, null, 'dircreated-'+outDirname); }
@@ -22,8 +30,8 @@ function useHtml(err, html, user, bytesWritten) {
   console.log('final result: %s, user: %s, written: %s', html, user, bytesWritten);     
 }
 
-var r = react('filename, uid, outDirname').define(
-    loadUser,         'uid              -> err, user',
+var r = react('filename, uid, outDirname, anObj').define(
+    'anObj.loadUser', 'uid              -> err, user',
     loadFile,         'filename         -> err, filedata',
     markdown,         'filedata         -> returns html',
     prepareDirectory, 'outDirname       -> err, dircreated', 
@@ -37,7 +45,8 @@ var r = react('filename, uid, outDirname').define(
 
 //console.log(r.ast);
 
-r.exec("hello.txt", 100, 'outHello', useHtml);
-r.exec("small.txt", 200, 'outSmall', useHtml);
-r.exec("world.txt", 300, 'outWorld', useHtml);
+var myObj = new UserMgr();
+r.exec("hello.txt", 100, 'outHello', myObj, useHtml);
+r.exec("small.txt", 200, 'outSmall', myObj, useHtml);
+r.exec("world.txt", 300, 'outWorld', myObj, useHtml);
 
