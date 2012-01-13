78648ddd1a2afffac48d020373bf1ae622dd8f6d
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-01-13 14:23:29 -0600

    add require('react/lib/promise-resolve'); which auto resolves promises that are passed in as arguments to the flow

---
+++ test/promise-auto-resolve.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+  Test that arguments which are promises are automatically resolved
+  before calling react functions
+  */
+
+var test = require('tap').test;
+var Deferred = require('promised-io/promise').Deferred;
+
+var react = require('../'); // require('react');
+require('../lib/promise-resolve');  // require('react/lib/promise-resolve');
+
+function multiply(x, y, cb) { cb(null, x * y); }
+function add(x, y, cb) { cb(null, x + y); }
+// function badF2(a, b, cb) { cb('my-error'); }
+
+test('auto resolve promises passed as args', function (t) {
+  t.plan(4);
+  var fn = react();
+  var errors = fn.setAndValidateAST({
+    inParams: ['a', 'b'],
+    tasks: [    
+      { f: multiply, a: ['a', 'b'], out: ['c'] },
+      { f: add, a: ['c', 'b'], out: ['d'] }
+    ],
+    outTask: { a: ['c', 'd'] }
+  });
+  t.deepEqual(errors, [], 'no validation errors');
+
+  function retAP() {
+    var deferred = new Deferred();
+    setTimeout(function () { deferred.resolve(2); }, 10);
+    return deferred.promise;
+  }
+
+  function retBP() {
+    var deferred = new Deferred();
+    setTimeout(function () { deferred.resolve(3); }, 10);
+    return deferred.promise;      
+  }
+
+  var ap = retAP();
+  var bp = retBP();
+
+  fn(ap, bp, function (err, c, d) {
+    t.equal(err, null);
+    t.equal(c, 6);
+    t.equal(d, 9);
+    t.end();
+  });
+});
